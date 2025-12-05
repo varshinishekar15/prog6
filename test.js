e65d8338abee6bf2ed81a6456548d501cc18ac50
Varshini Shekar <varshinishekar15@gmail.com>
2025-12-05 10:43:25 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,6 @@
-const request = require('supertest');
-const app = require('./app');
-
-describe('GET Routes Test Suite', () => {
-
-  it('GET / should return Hello From DSCE', async () => {
-    const res = await request(app).get('/');
-    if (res.text !== '{ "response": "Hello From DSCE" }') {
-      throw new Error('GET / failed');
-    }
-  });
-
-  it('GET /will should return Hello World', async () => {
-    const res = await request(app).get('/will');
-    if (res.text !== '{ "response": "Hello World" }') {
-      throw new Error('GET /will failed');
-    }
-  });
-
-  it('GET /ready should return Great!, It works!', async () => {
-    const res = await request(app).get('/ready');
-    if (res.text !== '{ "response": "Great!, It works!" }') {
-      throw new Error('GET /ready failed');
-    }
-  });
-
-});
+const express=require('express');
+const app=express();
+app.get('/', (req, res) => res.send('{ "response": "hello from DSCE" }'));
+app.get('/will', (req, res) => res.send('{ "response": "Hello World" }'));
+app.get('/ready', (req, res) => res.send('{ "response": "great, it works!" }'));
+module.exports = app;
